d8eaf13ef0d3893e049cdfbda3835abd55e0b652
idrivedigital <vishantdharmesh93@gmail.com>
2025-09-01 11:29:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "27263010"))
+    API_HASH = environ.get("API_HASH", "4c9da2b3fa15323237147eac02f1681c")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://vishantdharmesh0:v753V13ZGwJlFWbL@cluster0.wgfy7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 
@@ -27,3 +27,4 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
